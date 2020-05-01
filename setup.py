ce54c7337e7d988fae75d5f3348c3149e74e2a22
hack4lx <38162851+attakercyebr@users.noreply.github.com>
2020-05-01 13:38:06 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ def merge_csv():
 def update_tool():
 	import requests as r
 	banner()
-	source = r.get("https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/.image/.version")
+	source = r.get("ttps://raw.githubusercontent.com/attakercyebr/hack4lx_scraper_tg/master/.image/.version")
 	if source.text == '3':
 		print(gr+'['+cy+'+'+gr+']'+cy+' alredy latest version')
 	else:
@@ -86,11 +86,9 @@ def update_tool():
 		os.system('rm *.py');time.sleep(3)
 		print(gr+'['+cy+'+'+gr+']'+cy+' getting latest files ...')
 		os.system("""
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/add2group.py
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/scraper.py
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/setup.py
-			curl -s -O https://raw.githubusercontent.com/th3unkn0n/TeleGram-Scraper/master/smsbot.py
-			chmod 777 *.py
+			curl -s -O https://github.com/attakercyebr/hack4lx_scraper_tg/edit/master/scraper.py
+			curl -s -O https://github.com/attakercyebr/hack4lx_scraper_tg/edit/master/setup.py
+		        chmod 777 *.py
 			""");time.sleep(3)
 		print(gr+'\n['+cy+'+'+gr+']'+cy+' update compled.\n')
 
@@ -123,5 +121,5 @@ try:
 except IndexError:
 	print('\n'+gr+'['+re+'!'+gr+']'+cy+' no argument given : '+ sys.argv[1])
 	print(gr+'['+re+'!'+gr+']'+cy+' for help use : ')
-	print(gr+'['+re+'!'+gr+']'+cy+' https://github.com/th3unkn0n/TeleGram-Scraper#-how-to-install-and-use')
+	print(gr+'['+re+'!'+gr+']'+cy+' https://github.com/attakercyebr/hack4lx_scraper_tg/blob/master/README.md')
 	print(gr+'$ python3 setup.py -h'+'\n')
